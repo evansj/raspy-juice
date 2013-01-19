913c7491c8f5110c522a3ca23a7acf64e6c907df
Jon Evans <jon@springyweb.com>
2013-01-19 18:10:12 +0000

    Rework ADC interrupt code

--- firmware/svc-adc.c
+++ firmware/svc-adc.c
@@ -3,7 +3,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-volatile uint16_t adc[2]={15,30};
+volatile uint16_t adc[]={15,30};
 
 void adc_init()
 {
@@ -11,7 +11,10 @@ void adc_init()
     ADCSRA = (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
 
     // Right adjust for 8 bit resolution
-    ADMUX |= (1 << ADLAR);
+    // ADMUX |= (1 << ADLAR);
+
+    // Read ADC6 first
+    ADMUX |= (1<<MUX2) | (1<<MUX1);
 
     // Set auto-trigger enable
     ADCSRA |= (1 << ADATE);
@@ -41,11 +44,28 @@ void adc_stop()
 
 ISR(ADC_vect)
 {
-	if (ADMUX & MUX0) {
-		adc[1] = ADCH;
-		ADMUX &= ~(1 << MUX0);
-	} else {
-		adc[0] = ADCH;
-		ADMUX |= (1 << MUX0);
-	}
+	uint8_t mux, reg;           // temp registers for storage of misc data
+
+    mux = ADMUX;            	// read the value of ADMUX register
+    reg = mux & 0x0F;           // AND the first 4 bits (value of ADC pin being used)
+
+	uint16_t ADCval;
+	ADCval = ADCL;
+    ADCval = (ADCH << 8) + ADCval;    // ADCH is read so ADC can be updated again
+
+    if (reg == 6)
+    {
+        adc[0] = ADCval;
+        mux = ADMUX;
+        mux &= 0xF8; // clear last 3 bits
+        mux |= (1<<MUX2) | (1<<MUX1) | (1<<MUX0);
+        ADMUX = mux;
+    }
+    else if (reg == 7)
+    {
+        adc[1] = ADCval;
+        mux = ADMUX;
+        mux &= 0xF8; // clear last 3 bits
+        mux |= (1<<MUX2) | (1<<MUX1);
+    }
 }
